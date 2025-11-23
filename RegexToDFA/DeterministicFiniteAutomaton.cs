c6450cf217a515c5bdbb58d87f8482f77afdd9ec
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-11-23 11:47:26 +0200

    Initial commit

---
+++ RegexToDFA/DeterministicFiniteAutomaton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RegexToDFA
+{
+    public class DeterministicFiniteAutomaton
+    {
+
+        private HashSet<int> states { get; set; }
+        private HashSet<char> symbols { get; set; }
+        private Dictionary<int, Dictionary<char, int>> transitions { get; set; }
+        private int initialState { get; set; }
+        private HashSet<int> finalStates { get; set; }
+
+        public DeterministicFiniteAutomaton(int q0)
+        {
+            states = new HashSet<int>();
+            symbols = new HashSet<char>();
+            transitions = new Dictionary<int, Dictionary<char, int>>();
+            initialState = q0;
+            finalStates = new HashSet<int>();
+            
+            states.Add(initialState);
+        }
+
+        
+        public bool verifyAutomaton() {}
+        
+        public void printAutomaton()
+        {}
+        
+        public bool checkWord() {}
+    }
+}
---
+++ RegexToDFA/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RegexToDFA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
